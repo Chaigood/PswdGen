90af00d3b568c49bf0d82ba960d16011706ab330
Chaigood <cj.haigood@gmail.com>
2020-10-27 10:47:48 -0500

    broken but saving

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", ";", "/", "?", ".", ">", "<"];
-var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
+var specialChar = ["!@#$%^&*()_-+={}[];:'`~<,>.?/|"];
+var number = ["0123456789"];
 var pswdLength;
 var upperCaseVerify;
 var numberVerify;
 var specialVerify;
+var lowerCaseVerify;
 
 function SetLength(){
   pswdLength = prompt("How many characters do you want in your password?");
@@ -17,18 +18,21 @@ function SetLength(){
       SetLength();
     }
     else{
-      alert("Answer the next 3 questions to generate a password");
+      alert("Answer the next 4 questions to generate a password");
     }
     return pswdLength;
-}
+  }
 function SetUpperCase(){
-  upperCaseVerify = prompt("Do you want uppercase letters in the password?");
+  upperCaseVerify = confirm("Do you want uppercase letters in the password?");
 }
 function SetNumber(){
-  numberVerify = prompt("Do you want numbers in the password?");
+  numberVerify = confirm("Do you want numbers in the password?");
 }
 function SetSpecial(){
-  specialVerify = prompt("Do you want special characters in the password?");
+  specialVerify = confirm("Do you want special characters in the password?");
+}
+function SetLowerCase(){
+  lowerCaseVerify = confirm("Do you want lowercase letters in the password?");
 }
 
 function generatePassword(){
@@ -40,24 +44,41 @@ function generatePassword(){
   console.log(numberVerify);
   SetSpecial();
   console.log(specialVerify);
-var characters = lowerCase;
+  SetLowerCase();
+  console.log(lowerCaseVerify);
+  
+var characters;
 var password = "";
-if (upperCaseVerify && numberVerify && specialVerify){
+if (upperCaseVerify && numberVerify && specialVerify && lowerCaseVerify){
+  characters += upperCase + number + specialChar + lowerCase;
+}else if (upperCaseVerify && numberVerify && specialVerify){
   characters += upperCase + number + specialChar;
-}else if (upperCaseVerify && numberVerify){
-  characters += upperCase + number;
+}else if (upperCaseVerify && numberVerify && lowerCaseVerify){
+  characters += upperCase + number + lowerCase;
+}else if (upperCaseVerify && lowerCaseVerify && specialVerify){
+    characters += upperCase + lowerCase + specialChar;
+}else if (lowerCaseVerify && numberVerify && specialVerify){
+  characters += number + lowerCase + specialChar;
 }else if (numberVerify && specialVerify){
   characters += number + specialChar;
 }else if (upperCaseVerify && specialVerify){
   characters += upperCase + specialChar;
+}else if (lowerCaseVerify && specialVerify){
+  characters += lowerCase + specialChar;
+}else if (upperCaseVerify && lowerCaseVerify){
+  characters += upperCase + lowerCase;
+}else if (lowerCaseVerify && numberVerify){
+  characters += lowerCase + number;
+}else if (upperCaseVerify && numberVerify){
+  characters += upperCase + number;
 }else if (upperCaseVerify){
   characters += upperCase;
 }else if(numberVerify){
   characters += number;
 }else if (specialVerify){
   characters += specialChar;
-}else{
-  characters === lowerCase;
+}else {
+  characters === lowerCase; 
 }
   for(var i = 0; i < pswdLength; i++){
     password += characters.charAt(Math.floor(Math.random() * characters.length));
@@ -67,10 +88,11 @@ if (upperCaseVerify && numberVerify && specialVerify){
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password1 = "";
+  password1 = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password1;
 
 }
 
